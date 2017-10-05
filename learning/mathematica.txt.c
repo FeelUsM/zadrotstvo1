57d40403bd2387f4b810f58d2ab381841cd39526
FeelUs <fel1992@mail.ru>
2017-10-05 11:32:58 +0300

    contract.nb

--- learning/mathematica.txt.c
+++ learning/mathematica.txt.c
@@ -67,93 +67,90 @@ dd		DifferencialD
 \TeX
 \infty
 ->		Rule
+not		Not
 }
 {=== операторы ===
 http://reference.wolfram.com/language/tutorial/OperatorInputForms.html#6349
 (**) - комментарий
 () - группировка
 F[x,y] - функция
-{a,b,c} === List[a,b,c] - список
-"abc" === String["abc"] - строка ???
-x[[n,m,...]] === Part[x,n,m,...] - n-й элемент списка
-a;;b;;c === Span[a,b,c] - диапозон
-
-x=y === Set[x,y]
-x:=y === SetDelayed[x,y]
-x=. === Unset[x] (?)
-% === Out[] - последний результат
-expr1;expr2;expr3 === CompoundExpression[expr1,expr2,expr3]
-
-a+b+c === Plus[a,b,c]
-a-b === a+(-b)
--b === b*(-1)
-a/b === a*b^-1
-a*b*c === Times[a,b,c] ; пробел===*
-a^b === Power[a,b]
-a.^b === a*10^b
-x! === Factorial[x]
-x!! === Factorial2[x]
-x.y.z === Dot[x,y,z]
-
-x//F === F[x]
-f/@list === Map[f,list]
-f@@g[] === Apply[f,g[]]
-f@@@g[] === Apply[f,g[],{1}] --- на первом уровне
-body& === Function[body]
-#n === Slot[n]
-
-x_ === Pattern[x,Balnk[]]
-x->y === Rule[x,y]
-x:->y === RuleDelayed[x,y]
-expr/.rules === ReplaceAll[expr,rules]
-expr//.rules === ReplaceRepeat[expr,rules]
+f@g === f[g]
 }
 {=== Встроенные функции - с большй буквы ===
 	{ввод-вывод
 		In[]
+			% === Out[] - последний результат
 		Out[]
 		Quiet[выражение]	вычисляет выражение без вывода каких-либо сообщений
 		Off[Функция::тег]	выключение (подавление) сообщения
 		On[Функция::тег]	включение сообщения
 
+		70	x//f === Postfix[f[x]]
+		Postfix[]
+		Precedence[] - старшинство (для операторов?)
+		
 		MatrixForm[] - список в матрицу
 		StandardForm[] - человеческая форма
 		FullForm[] - машинная форма
 		InputForm[] - форма, которую снова можно скормить математике
 	}
 	{переменные, функции, опции, атрибуты
+		Hold[] - квотирование
+		40	x=y === Set[x,y]
 		Set[x,y] - вычислить y и в последсвии заменять x на этот вычисленный сейчас y
+		40	x:=y === SetDelayed[x,y]
 		SetDelayed[x,y] - не вычислять сейчас y и в последсвии заменять x на этот y, такой, какой он есть, а потом вычислять
+		670	x=. === Unset[x] (?)
 		Unset[]
-		Clear[] - сбрасывает определегние, "Global`*" - все текущие переменные
+		Clear[] - сбрасывает определение, "Global`*" - все текущие переменные
 		ClearAll[] - сбрасывает значения и атрибуты и ...
+		Remove[x] - удаляет x
+		
 		SetAttributes[f,attr] - устанавливает атрибут
 		Attributes[f] - возвращает список атрибутов
 		ClearAttributes[f] - удаляет аттрибуты
-		Remove[x] - удаляет x
+		HoldAll - не вычислять аргументы
 		Listable - атрибут, т.ч. f[{a,b,c}] -> {f[a],f[b],f[c]}
+		Orderless - атрибут, т.ч. f[a,b] == f[b,a]
+		Flat - атрибут т.ч. f[a,f[b]] == f[a,b]
+		OneIdentity - атрибут т.ч. f[x] == x
 		
 		Options[f] - список опций по умолчанию фуркции f
 	}
 	{строки
+			"abc" === String["abc"] - строка ???
 		String[]
 		ToString[x]
 		StringJoin[s1,s2,...]
 	}
+	{логика
+		230	!x === Not[x]
+		Not[]
+	}
 	{блоки, циклы и ветвления
-	
+		10	expr1;expr2;expr3 === CompoundExpression[expr1,expr2,expr3]
+		CompoundExpression[a,b,c] === a;b;c
+		                        m = i^2
+		Block[listVars,body]    Block[{i = a}, i + m] --> a+a^2 // dynamic scoping
+		Module[listVars,body]   Module[{i = a}, i + m] --> a+i^2// lexical scoping
+		With[listVars,body]
+		
+		While[cond,body]
 	}
 	{списки
+			{a,b,c} === List[a,b,c] - список
 		List[a,b,c] -> {a,b,c}
 		Range[n] -> {1,...,n}
 		Table[f[x],{x,xl,xh}] -> {f[xl], f[xl+1], ... , f[xh]}
 		Array[f,n] -> {f[1],...,f[n]}
 		
+			x[[n,m,...]] === Part[x,n,m,...] - n-й элемент списка
 		Part[l,n,m,...]
 		All - как индекс - выбрать все элементы
+		305	a;;b;;c === Span[a,b,c] - диапозон
 		Span[left,right,step]
 		Partition[l,n] - разбивает список l на список подсписков длиной n
-		Sort[v]
+		Sort[v] или ассоциацию
 		Union[v] - Sort Uniq
 		Reverse[]
 		RotateLeft[]
@@ -170,26 +167,70 @@ expr//.rules === ReplaceRepeat[expr,rules]
 		Delete[]
 		ReplacePart[]
 		Riffle[] - расставить между элементами другой элемент
+
+		OrderedQ[f[a,b,c]] - Упорядочены ли аргументы
+		Permutations[]
+		Signature[]
+		
+		Total[] - сумма (и с ассоц.)
+	}
+	{map, hash
+			<|a->1,b->2|> === Association[{a->1,b->2}]
+		Association[rules] (:> позволяет сохранять код и вызывать его)
+		Normal[] - Association -> List[rules]
+			ass[key] -> val // lvalue
+			ass[[Key[smth]]]
+			ass[["string"]]
+		Key[]?
+			
+		Select[ассоц,предикат]
+		Position[assoc,n] -> Key[...]
+		Append[assoc,rule]
+		Keys[assoc] -> {keys}
 	}
 	{функ.прог.
-		Map[f,{a,...,c}] -> {f[a],...,f[c]}
-		Apply[f,g[x,...,z]] -> f[x,...,z]
+		90	body& === Function[body]
 		Function[body] - лямбда-функция
+			#n === Slot[n]
 		Slot[n] - n-й аргумент лямбда-функции
+		620	f/@list === Map[f,list]
+		Map[f,{a,...,c}] -> {f[a],...,f[c]} или со значениями ассоциации
+		620	f@@g[] === Apply[f,g[]]
+			f@@@g[] === Apply[f,g[],{1}] --- на первом уровне
+		Apply[f,g[x,...,z]] -> f[x,...,z]
 		Nest[f,x,3] -> f[f[f[x]]]
 		NestList[]
 	}
 	{шаблоны и замены
+			x_ === Pattern[x,Balnk[]]
 		Pattern[] ???
+			_
 		Blank[]???
+			x__ === Pattern[x,BalnkSequence[]]
+		BlankSequence[]???
+		120	x->y === Rule[x,y]
 		Rule[]
+		120	x:>y === RuleDelayed[x,y]
 		RuleDelayed[] - правая часть квотирована
+		130	a/;cond
+		Condition[]
+		MatchQ[expr,rule(s)]
 		Replace[]
+		110	expr/.rules === ReplaceAll[expr,rules]
 		ReplaceAll[]
+		110	expr//.rules === ReplaceRepeated[expr,rules]
+		ReplaceRepeated[]
 	}
 	{арифметика
+		310	a+b+c === Plus[a,b,c]
+			a-b === a+(-b)
+			-b === b*(-1)
 		Plus[...]
+			a/b === a*b^-1
+		400	a*b*c === Times[a,b,c] ; пробел===*
 		Times[...]
+		590	a^b === Power[a,b]
+			a.^b === a*10^b
 		Power[a,b]
 		Sqrt[a]
 		Exp[a]
@@ -212,7 +253,9 @@ expr//.rules === ReplaceRepeat[expr,rules]
 		Min[a,b,c...]
 		Mod[a,b]
 		
+		610	x! === Factorial[x]
 		Factorial[x]
+			x!! === Factorial2[x]
 		Factorial2[x]
 
 		Pi
@@ -226,10 +269,6 @@ expr//.rules === ReplaceRepeat[expr,rules]
 		PrimeQ[x] - простое ли число
 		Prime[n] - n-е простое число
 	}
-	{Лин-Ал
-		Dot[] - умножение матриц
-		Eigenvalues[] - СЗ
-	}
 	{многочлены
 		Factor[expr] - разложить многочлен на множители
 		Expand[expr] - раскрыть скобки
@@ -238,6 +277,12 @@ expr//.rules === ReplaceRepeat[expr,rules]
 		Solve[lhs==rhs,x] - решить алгебраическое ур-е
 		NSolve[lhs==rhs,x] - численно решить алгебраическое ур-е
 		FindRoot[lhs==rhs,{x,x0}] - численно найти корень алгебраического ур-я около x0
+	}
+	{Лин-Ал
+		490	x.y.z === Dot[x,y,z]
+		Dot[] - умножение матриц
+		Eigenvalues[] - СЗ
+		Inverse[]
 	}
 	{матан
 		Limit[f,x->x0] - найти предел
